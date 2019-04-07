e21110aed1567a39b5c5039f1cf67c8796eaa8f9
延之 <yanzhi.cai@piaoniu.com>
2019-04-07 15:21:29 +0800

    基于循环数组实现一个先进先出的队列

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## 手动实现常见的数据结构和算法
 
-> **数据结构为算法服务**
+### 数据结构
 
 ##### 数组
 * <code>SimpleArray</code> 数组的插入,删除,按照下标随机访问
@@ -14,8 +14,14 @@
 * <code>LinkedListApp</code> 单链表应用
 	* <code>getMiddle()\*</code>	获取单链表中间节点（*\*号表示有多种实现*）
 	* <code>palindrome()</code> 判断链表是否为回文链表
+
 ##### 栈
 * <code>SimpleStack</code> 基于单链表实现的栈，提供入栈和出栈
 * <code>SampleBrowser</code> 基于栈模拟浏览器的前进和后退
 	* <code>printInverse()</code> 递归逆序打印节点值
 
+##### 队列
+* <code>SimpleArrayQueue</code> 基于数组实现先进先出的队列
+  *  实现为循环数组,入队出队只改变指针位置,不进行数据搬移
+
+### 算法
---
+++ queue/SimpleArrayQueue.java
@@ -0,0 +1,83 @@
+/**
+ * Created by evan on 2019/4/7.
+ *
+ * 基于数组实现先进先出的队列,
+ * 实现为循环数组,入队出队只改变指针位置,不进行数据搬移
+ */
+public class SimpleArrayQueue {
+
+    int[] data;
+
+    int offset;
+
+    int headIndex;
+
+    int length;
+
+    int validSpace;
+
+    public SimpleArrayQueue(int capacity) {
+        data = new int[capacity];
+        offset = 0;
+        headIndex = 0;
+        length = capacity;
+        validSpace = capacity;
+    }
+
+    //入队
+    public void enqueue(int value) {
+
+        if (validSpace == 0) {
+            System.out.println("队列满了");
+            return;
+        }
+
+        if (offset == length) {
+            offset = 0;
+        }
+
+        data[offset] = value;
+        validSpace--;
+        System.out.println("入队成功" + value + ",剩余空间:" + validSpace);
+        offset++;
+    }
+
+    //出队
+    public void dequeue() {
+
+        if (validSpace == length) {
+            System.out.println("队列为空");
+            return;
+        }
+
+        if (headIndex == length) {
+            headIndex = 0;
+        }
+
+        int res = data[headIndex];
+        validSpace++;
+        System.out.println("出队成功" + res + ",剩余空间:" + validSpace);
+        headIndex++;
+    }
+
+
+    public static void main(String[] args) {
+
+        SimpleArrayQueue queue = new SimpleArrayQueue(4);
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(4);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
+
+    }
+
+
+}
